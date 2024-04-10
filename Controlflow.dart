ad5f6e3348e04b195828de442153dd1d3efaa9ef
WaiyakiJonah <124672798+WaiyakiJonah@users.noreply.github.com>
2024-04-10 16:43:37 +0300

    Add files via upload

---
+++ Controlflow.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main() {
+  print("Enter a number: ");
+  String? input = stdin.readLineSync();
+
+  if (input != null && input.isNotEmpty) {
+    int number = int.tryParse(input)!;
+    if (number > 10) {
+      print("Your number is greater than 10");
+    } else if (number < 10) {
+      print("Your number is less than 10");
+    } else {
+      print("Your number is equal to 10");
+    }
+  } else {
+    print("No valid input provided.");
+  }
+}
